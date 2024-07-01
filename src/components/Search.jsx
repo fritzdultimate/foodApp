aaf806d512d9fe9e6c2e62cf749fdd433e330ed7
Nwosu Chukwuemeka Darlington <fritzdultimate@gmail.com>
2024-07-01 22:25:18 +0100

    search

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,10 +1,25 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-function Search({ name }) {
-  const [query, setQuery] = useState("")
+const URL = "https://api.spoonacular.com/recipes/complexSearch";
+const API_KEY = "90062ed8dfca4106967fd6e956d444bc";
+
+function Search({foodData, setFoodData}) {
+  const [query, setQuery] = useState("pizza");
+  useEffect(() => {
+    async function fetchFood() {
+      const res = await fetch(`${URL}?query=${query}&apiKey=${API_KEY}`);
+      const data = await res.json();
+      setFoodData(data.results);
+    }
+    fetchFood();
+  }, [query]);
   return (
     <div>
-      <input type="text" />
+      <input
+        type="text"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
     </div>
   );
 }
